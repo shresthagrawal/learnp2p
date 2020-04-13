b40a2989e8311eb2059f332f6075854a3e2a7ef5
Prateek Reddy <prateekreddyy@gmail.com>
2020-04-13 16:01:09 +0530

    Tutorial 3.0 updated with latest modules

--- assets/3.0-template-code.js
+++ assets/3.0-template-code.js
@@ -1,7 +1,6 @@
 'use strict'
 
-const util = require('util')
-const { createLibp2p } = require('libp2p')
+const Libp2p = require('libp2p')
 
 const TCP = require('libp2p-tcp')
 const WS = require('libp2p-websockets')
@@ -17,7 +16,14 @@ const WebrtcStar = new WStar({ wrtc: Wrtc })
 // TODO: modify options to include `connEncryption` and use Secio
 let options = {
     modules: {
-        transport: [ TCP, WS, WebrtcStar ]
+        transport: [ TCP, WS, WStar ]
+    },
+    config: {
+        transport: {
+            [transportKey]: {
+                Wrtc
+            }
+        }
     }
 }
 
@@ -31,22 +37,20 @@ async function onStart(libp2p) {
 
 async function main() {
     // Create a libp2p instance
-    let libp2p = await util.promisify(createLibp2p)(options)
+    let libp2p = await Libp2p.create(options)
 
-    libp2p.on('start', () => {
-        console.info(`Libp2p Started`)
-        libp2p.peerInfo.multiaddrs.forEach(ma => console.log(ma.toString()))
-        onStart(libp2p);
-    });
-
-    libp2p.on('connection:start', (peerInfo) => {
+    libp2p.on('peer:connect', (peerInfo) => {
         console.info(`Connected to ${peerInfo.id.toB58String()}!`)
     })
 
     libp2p.peerInfo.multiaddrs.add('/ip4/0.0.0.0/tcp/0')
     libp2p.peerInfo.multiaddrs.add('/ip4/0.0.0.0/tcp/0/ws')
 
-    await libp2p.start()
+    await libp2p.start();
+    console.info('Libp2p Started');
+    libp2p.peerInfo.multiaddrs.forEach(ma => console.log(ma.toString()));
+    onStart(libp2p);
 }
+
 
 main()
