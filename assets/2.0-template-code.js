3d2da352a85aa14bd3ac457d0a368f53cf01d2c4
Prateek Reddy <prateekreddyy@gmail.com>
2020-04-13 14:47:45 +0530

    Tutorial 2.0 updated with latest modules

--- assets/2.0-template-code.js
+++ assets/2.0-template-code.js
@@ -1,35 +1,36 @@
 'use strict'
 
-const util = require('util');
-const { createLibp2p } = require('libp2p')
+const Libp2p = require('libp2p')
 
 const TCP = require('libp2p-tcp')
 const WS = require('libp2p-websockets')
 const WStar = require('libp2p-webrtc-star')
 const Wrtc = require('wrtc')
 
-// Include multiaddr
-
-const WebrtcStar = new WStar({ wrtc: Wrtc })
+const transportKey = WStar.prototype[Symbol.toStringTag]
 
 let options = {
     modules: {
-        transport: [ TCP, WS, WebrtcStar ]
+        transport: [ TCP, WS, WStar ]
+    },
+    config: {
+        transport: {
+            [transportKey]: {
+                Wrtc
+            }
+        }
     }
 }
 
 async function main() {
     // Create a libp2p instance
-    let libp2p = await util.promisify(createLibp2p)(options)
-
-    libp2p.on('start', () => {
-        console.info(`Libp2p Started`)
-    })
+    let libp2p = await Libp2p.create(options)
 
     // Add multiaddr for TCP
     // Add multiaddr for ws
     
     await libp2p.start();
+    console.info(`Libp2p Started`)
 }
 
 main()
